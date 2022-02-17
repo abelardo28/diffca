1a1dcd2c64994cbe23494d463f35859394f2cc0e
abelardo28 <abelardo-ca@hotmail.com>
2022-02-17 15:37:46 -0600

    Alter table catalog values to indicators

--- app/Http/Livewire/Index.php
+++ app/Http/Livewire/Index.php
@@ -4,7 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Blog;
-use App\Models\CatalogValue;
+use App\Models\Indicator;
 
 class Index extends Component
 {
@@ -12,7 +12,7 @@ class Index extends Component
 
     public function mount(){
         $this->blogs = Blog::where('status', 1)->latest()->take(3)->get();
-        $this->values = CatalogValue::where('status', 1)->get();
+        $this->values = Indicator::where('status', 1)->get();
     }
 
     public function render()
--- app/Models/Indicator.php
+++ app/Models/Indicator.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class CatalogValue extends Model
+class Indicator extends Model
 {
     protected $fillable = ['type','description','value','status','updated_date'];
 }
